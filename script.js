be308f47e764be15a57b3449669c8a24ca12a60d
Tejas khupsare <152876278+tejaskhupsare@users.noreply.github.com>
2024-01-28 21:38:59 +0530

    project files uploaded

---
+++ script.js
@@ -0,0 +1,27 @@
+const generateBtn = document.querySelector(".generate-button");
+const memeTitle = document.querySelector(".meme-title");
+const memeImage = document.querySelector(".meme-image");
+const authorOutput = document.querySelector(".author");
+
+function getMeme() {
+
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((res) => res.json())
+    .then((data) => {
+
+      const { author, title, url } = data;
+      memeTitle.innerText = title;
+      memeImage.src = url;
+      authorOutput.innerText = `Meme by: ${author}`;
+      console.log(author, title, url);
+
+    });   
+}
+
+getMeme()
+
+generateBtn.addEventListener("click", () => {
+
+    getMeme()
+  
+});
